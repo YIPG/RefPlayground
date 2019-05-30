1b72278c5df238ef88dc4adeb16faf2a721c9614
yuya <u0ya317@gmail.com>
2019-05-30 20:47:24 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createRef } from "react";
 
-function App() {
+const Item = props => {
+  const { top, left } = props;
+  const [click, setClick] = useState(false);
+
+  useEffect(() => {
+    let itemRef = createRef();
+    props.focus && itemRef.current.focus();
+  }, [props.focus]);
+
+  const style = {
+    position: "absolute",
+    top: top,
+    left: left,
+    border: "1px solid",
+    height: "30px",
+    width: "30px"
+  };
+
+  const clickedStyle = {
+    ...style,
+    backgroundColor: "red"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      style={click ? clickedStyle : style}
+      tabIndex={0}
+      onClick={() => setClick(!click)}
+    />
   );
-}
+};
 
-export default App;
+const Grid = () => {
+  const [focusList, setFocusList] = useState(Array(25).fill(false));
+
+  const onHandleFocusList = i => {
+    setFocusList([...focusList.slice(0, i), true, focusList.slice(i + 1)]);
+  };
+
+  const pointList = [...Array(5)]
+    .map((_, i) => [...Array(5)].map((_, j) => [i * 40, j * 40]))
+    .flat();
+  // 5×5のグリッドを作成
+  // [[0,0],[0,40]...[40,0],[40,40]...[160,160]]
+
+  return pointList.map((topLeft, index) => (
+    <Item
+      focus={focusList[index]}
+      key={String(topLeft[0]) + String(topLeft[1])}
+      top={topLeft[0]}
+      left={topLeft[1]}
+    />
+  ));
+};
+
+export default Grid;
